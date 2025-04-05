7674a5cba8694235768693f4c38803cea29a5be2
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2025-04-05 17:55:14 -0300

    a

--- app.js
+++ app.js
@@ -8,32 +8,33 @@ function pesquisar() {
 
     let pesquisa = campoPesquisa.value.toLowerCase()
 
-    fetch("/api/dados.json", {method: "GET"})
-    .then(response => response.json())
-    .then(data => {console.log(data)})
-    // for (let dado of dados) {
-    //     titulo = dado.titulo.toLowerCase()
-    //     descricao = dado.descricao.toLowerCase()
+    fetch("/api/dados.json", { method: "GET" })
+        .then(response => response.json())
+        .then(data => {
+            for (let dado of data) {
+                titulo = dado.titulo.toLowerCase()
+                descricao = dado.descricao.toLowerCase()
 
-    //     if (titulo.includes(pesquisa) || descricao.includes(pesquisa)) {
-    //         resultado += `
-    //     <div class="item-resultado">
-    //         <h2>${dado.titulo}</h2>
-    //         <p class="descricao-meta">${dado.descricao}</p>
-    //         <img src="${dado.imagem}" alt="Uma foto de ${dado.titulo}">
-    //         <a href="${dado.link}" target="_blank">Mais informações sobre a obra da personagem </a>
-    //     </div>
-    // `
-    //     }
-    // }
-    
+                if (titulo.includes(pesquisa) || descricao.includes(pesquisa)) {
+                    resultado += `
+                 <div class="item-resultado">
+                     <h2>${dado.titulo}</h2>
+                     <p class="descricao-meta">${dado.descricao}</p>
+                     <img src="${dado.imagem}" alt="Uma foto de ${dado.titulo}">
+                     <a href="${dado.link}" target="_blank">Mais informações sobre a obra da personagem </a>
+                 </div>
+             `
+                }
+            }
+        })
+
     if (!pesquisa | pesquisa == "") {
-        resultado = "<p>Você não pesquisou nada, bocó!</p>" 
+        resultado = "<p>Você não pesquisou nada, bocó!</p>"
     }
 
     if (resultado == "") { // Antigamente eu estava verificando se a variável era indefinida, mas tinha algumas garotas que eu peguei da mesma obra, então as vezes elas citavam os mesmos nomes na descrição ou no título. Eu troquei para verificar uma string vazia.
         resultado = "<p> Esta personagem não existe, ou não consta na minha base de dados super avançada de última geração! </p>"
     }
-    
+
     section.innerHTML = resultado
 }
